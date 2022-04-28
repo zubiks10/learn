0e5ea6b41b9fa85d61cff85fe753a37e0e706cfe
zubiks10 <49195289+zubiks10@users.noreply.github.com>
2022-04-28 01:26:19 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "random_pet" "pet_name" {
+  length    = 3
+  separator = "-"
+}
+
+resource "aws_iam_user_policy_attachment" "attachment" {
+  user       = aws_iam_user.new_user.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_iam_user" "new_user" {
+  name = "new_user"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${random_pet.pet_name.id}-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  acl = "private"
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "${random_pet.pet_name.id}-policy"
+  description = "My test policy"
+
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:ListAllMyBuckets"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    },
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.bucket.arn}"
+    }
+
+  ]
+}
+EOT
+}
+data "aws_iam_policy_document" "example" {
+  statement {
+    actions   = ["s3:ListAllMyBuckets"]
+    resources = ["arn:aws:s3:::*"]
+    effect = "Allow"
+  }
+  statement {
+    actions   = ["s3:*"]
+    resources = [aws_s3_bucket.bucket.arn]
+    effect = "Allow"
+  }
+}
+
+output "rendered_policy" {
+  value = data.aws_iam_policy_document.example.json
+}
+
+
